206e47944c0a0dcae526dbb516d71c990e308595
tharukavishwajiths@gmail.com <tharukavishwajiths@gmail.com>
2022-11-23 12:34:27 +0530

    added search controller

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from wsgiref.simple_server import make_server
 import falcon
+import json
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    OrderedDict = dict
 
 import sentences_data
+
+Large_SEARCHER_Model = 'all-MiniLM-L6-v2'
+SMALL_SEARCHER_Model = 'paraphrase-albert-small-v2'
+
+ERR_NOT_SUPPORTED = {"status": falcon.HTTP_404,
+                     "code": 10, "title": "Not Supported"}
+
+ERR_UNKNOWN = {"status": falcon.HTTP_500,
+               "code": 500, "title": "Unknown Error"}
 
 app = falcon.App(middleware=[
 ])
@@ -14,9 +29,89 @@ class Greeting:
         resp.media = {'message': "Hello World!"}
 
 
+class Searcher:
+    def __init__(self, model_name):
+        self.model = model_name
+
+    def search(self, query):
+        print(query)
+        return {'model': self.model}
+
+
+class LargeSearcher(Searcher):
+    def __init__(self):
+        super().__init__(Large_SEARCHER_Model)
+
+
+class SmallSearcher(Searcher):
+    def __init__(self):
+        super().__init__(SMALL_SEARCHER_Model)
+
+
+large_searcher = LargeSearcher()
+small_searcher = SmallSearcher()
+
+
+class SearchController:
+    def on_get(self, req, resp):
+        print(req.params['model'])
+        if "small" == req.params['model']:
+            resp.media = large_searcher.search("test")
+        elif "large" == req.params['model']:
+            resp.media = small_searcher.search("test")
+        else:
+            resp.media = {"message": "No Model Found"}
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+
+
+# class AppError(Exception):
+#     def __init__(self, error=ERR_UNKNOWN, description=None):
+#         self.error = error
+#         self.error["description"] = description
+
+#     @property
+#     def code(self):
+#         return self.error["code"]
+
+#     @property
+#     def title(self):
+#         return self.error["title"]
+
+#     @property
+#     def status(self):
+#         return self.error["status"]
+
+#     @property
+#     def description(self):
+#         return self.error["description"]
+
+#     @staticmethod
+#     def handle(exception, req, res, error=None):
+#         res.status = exception.status
+#         meta = OrderedDict()
+#         meta["code"] = exception.code
+#         meta["message"] = exception.title
+#         if exception.description:
+#             meta["description"] = exception.description
+#         res.body = json.dumps({"meta": meta})
+
+
+# class NotSupportedError(AppError):
+#     def __init__(self, method=None, url=None):
+#         super().__init__(ERR_NOT_SUPPORTED)
+#         if method and url:
+#             self.error["description"] = "method: %s, url: %s" % (method, url)
+
+
 greeting = Greeting()
 
+search_controller = SearchController()
+
 app.add_route('/', greeting)
+app.add_route('/search', search_controller)
+
+# app.add_error_handler(falcon.HTTPNotFound, NotSupportedError.handle)
 
 if __name__ == '__main__':
     with make_server('', 8000, app) as httpd:
